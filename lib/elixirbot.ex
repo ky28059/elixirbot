9410662b0b87dddf06150d040d02bf492843a9b2
Kevin Yu <kevinyu567@gmail.com>
2023-04-03 17:20:33 -0700

    Initial commit

---
+++ lib/elixirbot.ex
@@ -0,0 +1,18 @@
+defmodule Elixirbot do
+  @moduledoc """
+  Documentation for `Elixirbot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirbot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
