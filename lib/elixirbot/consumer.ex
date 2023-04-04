0e602aff107d5e9e583428c34888fd59c7f521e3
Kevin Yu <kevinyu567@gmail.com>
2023-04-03 21:12:27 -0700

    Use embed for pfp, increase image size

--- lib/elixirbot/consumer.ex
+++ lib/elixirbot/consumer.ex
@@ -7,15 +7,17 @@ defmodule Elixirbot.Consumer do
   use Nostrum.Consumer
 
   alias Nostrum.Api
-  alias Nostrum.Struct.User
+  alias Nostrum.Struct.{Embed, User}
 
   def start_link do
     Consumer.start_link(__MODULE__)
   end
 
   def handle_event({:READY, data, _ws_state}) do
-    Logger.info("Logged in as #{data.user.username}##{data.user.discriminator}!")
-    Api.update_status(:online, "the competition", 5)  # https://discord.com/developers/docs/game-sdk/activities#data-models-activitytype-enum
+    Logger.info("Logged in as #{User.full_name(data.user)}!")
+
+    # https://discord.com/developers/docs/game-sdk/activities#data-models-activitytype-enum
+    Api.update_status(:online, "the competition", 5)
   end
 
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
@@ -24,9 +26,18 @@ defmodule Elixirbot.Consumer do
         Api.create_message(msg.channel_id, "pong!")
 
       "!pfp" ->
+        # Semi-hacky workaround to get larger pfp size (not supported by nostrum as it is by discord.js)
+        icon_url = User.avatar_url(msg.author) <> "?size=4096"
+
+        embed = %Embed{}
+          |> Embed.put_author(User.full_name(msg.author), nil, icon_url)
+          |> Embed.put_image(icon_url)
+          |> Embed.put_color(0x6e4a7e)
+          |> Embed.put_footer("Requested by #{User.full_name(msg.author)}", nil)
+
         Api.create_message(
           msg.channel_id,
-          content: User.avatar_url(msg.author),
+          embeds: [embed],
           message_reference: %{message_id: msg.id},
           allowed_mentions: :none
         )
